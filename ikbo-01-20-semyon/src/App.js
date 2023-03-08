d797e10aee755bc7e5a857acc68931433647e914
Semyon Esaev <semen.esaev@yandex.ru>
2023-03-08 09:30:04 +0300

    tutorial-08-03-2023: create new own component - Select, add sorting by title, body with using memoization in hook 'useMemo'

--- ikbo-01-20-semyon/src/App.js
+++ ikbo-01-20-semyon/src/App.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import '../src/styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import Select from "./components/UI/select/Select";
+import Input from "./components/UI/input/Input";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,6 +14,16 @@ function App() {
         ]
     );
 
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const sortedPosts = useMemo(() => {
+        console.log("Получение отсортированных постов");
+        return selectedSort
+            ? [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+            : posts;
+    }, [selectedSort, posts])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
     }
@@ -20,11 +32,31 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px'}}/>
+            <div>
+                <Input
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="Поиск"
+                />
+                <Select
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка по"
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'},
+                    ]}/>
+            </div>
             {posts.length !== 0
-                ? <PostList remove={removePost} posts={posts} title={"Список постов"}/>
+                ? <PostList remove={removePost} posts={sortedPosts} title={"Список постов"}/>
                 : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
             }
         </div>
