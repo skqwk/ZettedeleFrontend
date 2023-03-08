2f5e0bcf519b927d37ad3ab2e6b56bdfd314122c
Semyon Esaev <semen.esaev@yandex.ru>
2023-03-08 08:58:24 +0300

    tutorial-08-03-2023: add function removePost

--- ikbo-01-20-semyon/src/App.js
+++ ikbo-01-20-semyon/src/App.js
@@ -16,10 +16,17 @@ function App() {
         setPosts([...posts, newPost]);
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title={"Список постов"}/>
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={posts} title={"Список постов"}/>
+                : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
+            }
         </div>
     );
 }
