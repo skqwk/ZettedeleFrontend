bbc8ba4b3bb57e9452b2394451f800b966e7f73a
Semyon Esaev <semen.esaev@yandex.ru>
2023-03-09 07:20:37 +0300

    tutorial-08-03-2023: install axios for performing requests, add 'useEffect' for starting load data from server

--- ikbo-01-20-semyon/src/App.js
+++ ikbo-01-20-semyon/src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../src/styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,23 +6,28 @@ import PostFilter from "./components/PostFilter";
 import Modal from "./components/UI/modal/Modal";
 import Button from "./components/UI/button/Button";
 import {usePosts} from "./hooks/usePost";
+import axios from 'axios';
 
 function App() {
-    const [posts, setPosts] = useState([
-            {id: 1, title: 'Java Script', 'body': 'Description'},
-            {id: 2, title: 'Python', 'body': 'Description'},
-            {id: 3, title: 'C++', 'body': 'Description'},
-            {id: 4, title: 'Dart', 'body': 'Description'},
-        ]
-    );
+    const SERVER = 'https://jsonplaceholder.typicode.com';
 
+    const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get(`${SERVER}/posts`)
+        setPosts(response.data);
     }
 
     const removePost = (post) => {
