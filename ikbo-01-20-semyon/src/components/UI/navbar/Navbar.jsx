108cc323c121c16b96bafd2cceca39810716bd43
Semyon Esaev <semen.esaev@yandex.ru>
2023-03-10 08:04:01 +0300

    tutorial-08-03-2023: add logout button on navbar

--- ikbo-01-20-semyon/src/components/UI/navbar/Navbar.jsx
+++ ikbo-01-20-semyon/src/components/UI/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
+import Button from "../button/Button";
+import {AuthContext} from "../../../context";
 
 const Navbar = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+
+    const logout = (e) => {
+        e.preventDefault();
+        setIsAuth(false);
+    }
+
     return (
         <div className="navbar">
+            <Button onClick={logout}>Выйти</Button>
             <div className="navbar__links">
                 <Link to="/about">О сайте</Link>
                 <Link to="/posts">Посты</Link>
