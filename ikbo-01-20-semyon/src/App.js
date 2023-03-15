7d116d40c38944fbfb5ccd7f26e151bdd530b37b
Semyon Esaev <semen.esaev@yandex.ru>
2023-03-15 22:28:25 +0300

    practice-2: add circles on background with blur effect

--- ikbo-01-20-semyon/src/App.js
+++ ikbo-01-20-semyon/src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.png';
 import './App.css';
 import React from 'react';
 
 const App = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Простое React-приложение
-                </p>
-            </header>
+            <div className="about-container">
+                <h1>Zettedele</h1>
+                <ul className="navbar">
+                    <li className="navbar-item">Профиль</li>
+                    <li className="navbar-item">Хранилища</li>
+                    <li className="navbar-item">Поиск</li>
+                </ul>
+                <div className="frozen-container">
+                    <div className="content">
+                        <p className="text__center">Zettedele - это система ведения заметок и управления личными
+                            знаниями,
+                            построенная на основе метода
+                            Zettelkasten
+                        </p>
+                        <p className="text__center">
+                            Важные мысли не потеряются: каждую заметку можно связать ссылкой с другой заметкой,
+                            использование
+                            графа для визуализации
+                            гарантирует наглядность базы знаний, а система тегов позволяет классифицировать заметки.
+                        </p>
+                    </div>
+                    <div className="circle-blue"></div>
+                    <div className="circle-pink"></div>
+                </div>
+
+                <div className="box">
+                    <div className="box-content">
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
