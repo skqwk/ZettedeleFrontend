042f95e077175244cc48061ba16434fda82e46f5
Semyon Esaev <semen.esaev@yandex.ru>
2023-03-16 08:24:42 +0300

    practice-3: add routing

--- ikbo-01-20-semyon/src/App.js
+++ ikbo-01-20-semyon/src/App.js
@@ -1,43 +1,19 @@
 import './App.css';
-import React, {useEffect} from 'react';
+import React from 'react';
+import {BrowserRouter, Link} from "react-router-dom";
+import AppRouter from "./AppRouter";
 import Navbar from "./Navbar";
 
 const App = () => {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "./mouseTracker.js"
-        document.body.appendChild(script)
-        return () => {
-            // clean up the script when the component in unmounted
-            document.body.removeChild(script)
-        }
-    }, [])
-
     return (
         <div className="App">
-            <div className="about-container">
-                <h1>Zettedele</h1>
+            <BrowserRouter>
+                <Link to="/about" style={{ textDecoration: 'none' }}>
+                    <h1>Zettedele</h1>
+                </Link>
                 <Navbar/>
-                <div className="frozen-container">
-                    <div className="content">
-                        <p className="text__center">Zettedele - это система ведения заметок и управления личными
-                            знаниями,
-                            построенная на основе метода
-                            Zettelkasten
-                        </p>
-                        <br/>
-                        <p className="text__center">
-                            Важные мысли не потеряются: каждую заметку можно связать ссылкой с другой заметкой,
-                            использование
-                            графа для визуализации
-                            гарантирует наглядность базы знаний, а система тегов позволяет классифицировать заметки.
-                        </p>
-                    </div>
-                    <div className="circle-blue"></div>
-                    <div className="circle-pink"></div>
-                </div>
-
-            </div>
+                <AppRouter/>
+            </BrowserRouter>
         </div>
     );
 }
